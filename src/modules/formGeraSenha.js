dbbd5affbbd4d69510215aa853a2e9b6d9fbec3b
Lul4t3ch <Lul4t3ch15@gmail.com>
2022-12-28 11:05:21 -0300

    first commit

---
+++ src/modules/formGeraSenha.js
@@ -0,0 +1,23 @@
+import geraSenha from './geradores';
+
+const senhaGerada = document.querySelector('.senha-gerada');
+const qtdCaracteres = document.querySelector('.qtd-caracteres');
+const chkMaiusculas = document.querySelector('.chk-maiusculas');
+const chkMinusculas = document.querySelector('.chk-minusculas');
+const chkNumeros = document.querySelector('.chk-numeros');
+const chkSimbolos = document.querySelector('.chk-simbolos');
+const button = document.querySelector('.gerar-senha');
+
+export default () => {
+    button.addEventListener('click', (e) => {
+        senhaGerada.innerHTML = gera();
+    })
+};
+
+const gera = () => {
+    const senha = geraSenha(qtdCaracteres.value, 
+        chkMaiusculas.checked, chkMinusculas.checked,
+        chkNumeros.checked, chkSimbolos.checked);
+
+    return senha || 'Selecione uma das opções';
+};
